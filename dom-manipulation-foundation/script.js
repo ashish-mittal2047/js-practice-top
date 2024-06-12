787e74af3b703b940f869bb5414a6bde926b358b
ashish.mittal2047 <68066532+ashish-mittal2047@users.noreply.github.com>
2024-06-12 17:54:08 +0530

    Create div and its elements and insert text

--- dom-manipulation-foundation/script.js
+++ dom-manipulation-foundation/script.js
@@ -1,5 +1,19 @@
 const para = document.createElement('p');
 para.classList.add('red-text');
+para.textContent = "Hey I'm red!";
 
-const heading3 = document.createElement('h3');
-heading3.classList.add('blue-text');
+const heading = document.createElement('h3');
+heading.classList.add('blue-text');
+heading.textContent = "I'm a blue h3!";
+
+const div = document.createElement('div');
+div.classList.add('black-border');
+div.classList.add('background-pink');
+
+const headingInsideDiv = document.createElement('h1');
+headingInsideDiv.textContent = "I'm in a div";
+
+const paraInsideDiv = document.createElement('p');
+paraInsideDiv.classList.add("ME TOO!");
+
+
